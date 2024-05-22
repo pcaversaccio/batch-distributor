13c5b4597a5ef1a636b91312f8dc933f1d3bbfb7
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-05-22 17:47:28 +0200

    🔁 Upgrade to Solidity Version `0.8.26`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Source: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/mocks/token/ERC20Mock.sol
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.26;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
