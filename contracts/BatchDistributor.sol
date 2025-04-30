29183d151e6f0cf88bd8194175c5309b8fbe0e23
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2025-04-30 16:48:00 +0200

    🔁 Update `prettier-plugin-solidity` to Version `2.0.0`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- contracts/BatchDistributor.sol
+++ contracts/BatchDistributor.sol
@@ -2,7 +2,9 @@
 pragma solidity 0.8.29;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {
+    SafeERC20
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
  * @dev Error that occurs when transferring ether has failed.
